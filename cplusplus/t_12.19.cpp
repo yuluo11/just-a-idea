b57b446ba6f8e2a4fde164b3818308de59880fb3
yura luan <2167388292@qq.com>
2025-12-19 17:02:53 +0800

    string and ptr

---
+++ cplusplus/t_12.19.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string str1={"yura luan"};
+    //cout<<str1<<endl;
+    string str2;
+    cin>>str2;
+    //cout<<str2<<endl;
+    string str3;
+    str3=str1+str2;
+    str1+=str2;
+    cout<<str3<<endl<<str1<<endl;
+    
+    return 0;
+}
